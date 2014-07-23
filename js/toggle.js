af9c91cb5cc10845b0046212887605c77f8fade2
deanrussell <deanwagnerrussell@gmail.com>
2014-07-23 00:24:34 -0400

    updated off-canvases

--- js/toggle.js
+++ js/toggle.js
@@ -11,4 +11,14 @@ $(document).ready(function(){
         $(".cover").toggleClass("cover__page-left");
         $(".off-right").toggleClass("on-left");
     });
+    $("#bio-click").click(function(){
+        $("#bio-click-return").toggleClass("bio-button__sprite");
+        $("#bio-click-return").toggleClass("bio-button__sprite-x");
+        $("#b1").toggleClass("bio-hide01");
+        $("#b2").toggleClass("bio-hide02");
+        $("#b3").toggleClass("bio-hide03");
+        $("#b4").toggleClass("bio-hide04");
+        $("#b5").toggleClass("bio-hide05");
+        $("#b6").toggleClass("bio-hide06");
+    });
 });
